078ca28f781788a214e8145caba15af3f1022743
trebuhw <114191788+trebuhw@users.noreply.github.com>
2025-04-27 14:55:22 +0200

    up ins-scr

--- OpenSuse-install.sh
+++ OpenSuse-install.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 # Install log save
-exec > "$HOME/plik_log.txt" 2>&1
+# exec > "$HOME/plik_log.txt" 2>&1
 
 # packer="sudo zypper -n install" # Wersja skrócona polecenia
 packer="sudo zypper --non-interactive install --no-recommends"
 
-$packer alacritty bash-completion bat blueman brightnessctl btop cpuid cups curl dconf-editorduf dunst fastfetch feh file-roller firefox fish flameshot font-manager fzf galculator gcc gcolor2 gimp git gnome-disk-utility gparted gsettings-desktop-schemas gzip harfbuzz-devel htop i3lock kitty libreoffice libreoffice-l10n-pl libX11-devel libXft-devel libXinerama-devel libxcb-res0 lightdm lightdm-gtk-greeter-settings lsd mako meld mlocate MozillaThunderbird neovim NetworkManager-applet ncurses-devel numlockx nwg-look opi os-prober p7zip papirus-icon-theme parcellite pavucontrol pdfarranger picom polkit polkit-gnome ranger rclone ripgrep rofi rsync scrot sensors starship stow sxhkd sxiv system-config-printer tealdeer tealdeer-fish-completion thunar thunar-volman time tree ueberzugpp unrar unzip vim vlc wget xclip xdg-user-dirs xfce4-notifyd xinit xorg-x11-driver-video xorg-x11-essentials xorg-x11-fonts xorg-x11-fonts-converted xorg-x11-fonts-core xorg-x11-fonts-legacy xorg-x11-libX11-ccache xorg-x11-server xorg-x11-server-extra xorg-x11-server-Xvfb xorg-x11-Xvnc xorg-x11-Xvnc-module xorgproto-devel xwininfo yazi zathura zathura-plugin-pdf-poppler zoxide
+$packer alacritty bash-completion bat blueman brightnessctl btop cpuid cups curl dconf-editorduf dunst fastfetch feh file-roller firefox fish flameshot font-manager fzf galculator gcc gcolor2 gimp git gnome-disk-utility gparted gsettings-desktop-schemas gzip harfbuzz-devel htop i3lock kitty libreoffice libreoffice-l10n-pl libX11-devel libXft-devel libXinerama-devel libxcb-res0 mako meld mlocate MozillaThunderbird neovim NetworkManager-applet ncurses-devel numlockx nwg-look opi os-prober p7zip papirus-icon-theme parcellite pavucontrol pdfarranger picom polkit polkit-gnome ranger rclone ripgrep rofi rsync scrot sensors starship stow sxhkd sxiv system-config-printer tealdeer tealdeer-fish-completion thunar thunar-volman time tree ueberzugpp unrar unzip vim vlc wget xclip xdg-user-dirs xfce4-notifyd xinit xorg-x11-driver-video xorg-x11-essentials xorg-x11-fonts xorg-x11-fonts-converted xorg-x11-fonts-core xorg-x11-fonts-legacy xorg-x11-libX11-ccache xorg-x11-server xorg-x11-server-extra xorg-x11-server-Xvfb xorg-x11-Xvnc xorg-x11-Xvnc-module xorgproto-devel xwininfo yazi zathura zathura-plugin-pdf-poppler zoxide
 
 # Nvidia add repository
-$packer openSUSE-repos-Tumbleweed-NVIDIA 
+sudo zypper addrepo --refresh https://download.nvidia.com/opensuse/tumbleweed NVIDIA
 sudo zypper refresh
+sudo zypper dup
 
 # Install Drives
-$packer kernel-firmware-nvidia libnvidia-egl-wayland1 nvidia-compute-G06 nvidia-compute-G06-32bit nvidia-driver-G06-kmp-default nvidia-gl-G06 nvidia-gl-G06-32bit nvidia-video-G06 nvidia-video-G06-32bit openSUSE-repos-MicroOS-NVIDIA xf86-video-intel
+$packer kernel-firmware-nvidia libnvidia-egl-wayland1 nvidia-compute-G06 nvidia-compute-G06-32bit nvidia-driver-G06-kmp-default nvidia-gl-G06 nvidia-gl-G06-32bit nvidia-video-G06 nvidia-video-G06-32bit xf86-video-intel
 
 # OPI APP
 opi google-chrome
 opi trash-cli
 opi sublime
-opi joplin-desktop
 
 # install QUEMU
 $packer -t pattern kvm_server kvm_tools
@@ -37,9 +37,9 @@ systemctl start avahi-daemon.service
 systemctl enable avahi-daemon.service
 
 # Install Github Desktop
-sudo rpm --import https://rpm.packages.shiftkey.dev/gpg.key
-sudo sh -c 'echo -e "[shiftkey-packages]\nname=GitHub Desktop\nbaseurl=https://rpm.packages.shiftkey.dev/rpm/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://rpm.packages.shiftkey.dev/gpg.key" > /etc/zypp/repos.d/shiftkey-packages.repo'
-sudo zypper ref && sudo zypper in github-desktop
+#sudo rpm --import https://rpm.packages.shiftkey.dev/gpg.key
+#sudo sh -c 'echo -e "[shiftkey-packages]\nname=GitHub Desktop\nbaseurl=https://rpm.packages.shiftkey.dev/rpm/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://rpm.packages.shiftkey.dev/gpg.key" > /etc/zypp/repos.d/shiftkey-packages.repo'
+#sudo zypper ref && sudo zypper in github-desktop
 
 # File Coopy
 # sudo mv /etc/tlp.conf /etc/tlp.conf.bak
@@ -50,12 +50,6 @@ sudo cp -r ~/dwm/files/usr/share/icons/{.,}* /usr/share/icons
 sudo cp -r ~/dwm/files/usr/share/themes/{.,}* /usr/share/themes
 sudo cp ~/dwm/files/etc/X11/xorg.conf.d/20-intel.conf /etc/X11/xorg.conf.d
 sudo cp ~/dwm/files/etc/tlp.conf /etc
-
-#Lightdm
-sudo mkdir /usr/share/backgrounds
-sudo cp ~/dwm/files/.config/suckless/city-nord.png /usr/share/backgrounds/
-sudo systemctl enable lightdm.services
-sudo systemctl start lightdm.services
 
 # TLP
 sudo cp ~/dwm/files/etc/tlp.conf /etc
